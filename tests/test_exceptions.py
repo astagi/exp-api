c49b42d03c030482482ac1541fa8e1ed8564ef0a
Andrea Stagi <stagi.andrea@gmail.com>
2015-01-29 00:40:36 +0100

    Fix exceptions tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -8,10 +8,10 @@ class TestExceptions(unittest.TestCase):
         error_message = '{"_error_type": "taiga.base.exceptions.WrongArguments", "_error_message": "Username or password does not matches user."}'
         taiga_exception = taiga.exceptions.TaigaRestException(
             'uri', 500, error_message)
-        self.assertEqual(taiga_exception.message, 'Username or password does not matches user.')
+        self.assertEqual(str(taiga_exception), 'Username or password does not matches user.')
 
     def test_taiga_rest_exception_parsing_wrong_json(self):
         error_message = 'Plain message error.'
         taiga_exception = taiga.exceptions.TaigaRestException(
             'uri', 500, error_message)
-        self.assertEqual(taiga_exception.message, 'Plain message error.')
+        self.assertEqual(str(taiga_exception), 'Plain message error.')
